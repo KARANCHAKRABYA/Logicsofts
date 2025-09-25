64dbc123147c409072d909a865394ca793d063f1
Karan Kumar <107801028+KARANCHAKRABYA@users.noreply.github.com>
2025-09-25 16:39:39 +0530

    Create Redirect to another page .htaccess code.php

---
+++ Redirect to another page .htaccess code.php
@@ -0,0 +1,14 @@
+# Redirect /gallery to birmingham-gallery
+Redirect 301 /gallery/ /birmingham-gallery/
+
+# Redirect /more to birmingham-gallery
+Redirect 301 /more https://exxxclusiveescorts.co.uk/birmingham-gallery/
+
+# Redirect specific escort profile to birmingham-gallery
+Redirect 301 /escort/ruby-exclusive-elegant-unforgettable/ https://exxxclusiveescorts.co.uk/birmingham-gallery/
+
+# Redirect escort Karla-2 to Karla
+Redirect 301 /escort/karla-2/ https://exxxclusiveescorts.co.uk/escort/karla/
+
+# Redirect Aldridge location to Walsall Escorts location
+Redirect 301 /location/aldridge/ https://exxxclusiveescorts.co.uk/location/walsall-escorts/
